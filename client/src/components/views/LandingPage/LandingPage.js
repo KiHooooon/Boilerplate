b44a0ec7ee01c45e7e2e0d147493b5a9f2da5cda
KiHooooon <hksy113@gmail.com>
2020-12-02 21:53:48 +0900

    DB에서 가져온 Product 데이터를 가지고 Product 이미지 카드 생성

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,23 +1,49 @@
-import React, {useEffect} from 'react'
+import React, {useEffect,useState} from 'react'
 import { FaCode } from "react-icons/fa";
 import axios from 'axios';
+import { Icon, Col, Card, Row, Carousel } from 'antd';
+import Meta from 'antd/lib/card/Meta';
 
 function LandingPage() {
-
+    const [Products, setProducts] = useState([]);
     useEffect(() => {
         axios.post('api/product/products')
             .then(response => {
                 if(response.data.success) {
                     console.log(response.data);
+                    setProducts(response.data.products);
                 } else {
-
+                    console.log(response.data.err);
                 }
             })
 
         
     }, [])
+
+    const renderCards = Products.map((product, index) => {
+        return (
+            <Col lg={6} md={8} xs={24} key={index}>
+                <Card        
+                    cover={<img src={`http://localhost:5000/${product.images[0]}`} />}
+                >
+                    <Meta
+                        title={product.title}
+                        description={`$${product.price}`}
+                    />
+                </Card>        
+            </Col>
+        )
+    })
     return (
-        <div>LandingPage</div>
+        <div style={{ width: '75%', margin: '3rem auto' }}>
+            <div style={{ textAlign: 'center' }}>
+                <h2>Let's Travel Anywhere <Icon type="rocket" /> </h2>
+            </div>
+        
+            <Row gutter={16, 16}>
+                {renderCards}
+            </Row>
+        </div>
     )
 }
 
